21b165d4f3ea9880c0dd69a2c8dca7d60b938461
saurabdixit <saurabh.dxt21@gmail.com>
2015-09-03 19:10:22 -0700

    fixed file handling

--- grasp_manager/scripts/convert_avi.py
+++ grasp_manager/scripts/convert_avi.py
@@ -1,13 +1,27 @@
 #! /usr/bin/env python
 import os
 from shared_playback import *
+import time
 
 if __name__ == "__main__":
     dirs = get_data_dirs(grasp_data_directory)
     for (data_dir, obj_num, sub_num) in dirs:
             smi_robot_path = data_dir + "/" + "smi_robot"
-            if not os.exists(smi_robot_path):
+            if not os.path.exists(smi_robot_path):
                 print "No data directory in ", smi_robot_path
                 continue
-            os.system("avconv *recording.avi -c:a Mp4_format_recording.mp4")
+            list_dir = os.listdir(smi_robot_path)
+            filename = ''
+            filename_list = [s for s in list_dir if "recording.avi" in s]
+            remove_filename = [s for s in list_dir if "recording.avi.rmf" in s]
+            if len(filename_list) != 0 and len(remove_filename) != 0:
+                filename_list.remove(remove_filename[0])
+            if len(filename_list) != 0:
+                filename = smi_robot_path + "/"+filename_list[0]
+                outfile = smi_robot_path + "/Mp4_format_recording.mp4"
+            print filename
+            print outfile
+            if len(filename) != 0:
+                os.system("avconv -i "+filename+" -c:a copy "+outfile)
+                time.sleep(10)
 
