8df725308aaf15a768ed78814721f046266047aa
Jackson Carter <cartejac@onid.oregonstate.edu>
2016-06-23 09:47:57 -0700

    Final commit for this branch... Seniors are done.

--- grasp_manager/scripts/shared_globals.py
+++ grasp_manager/scripts/shared_globals.py
@@ -1,7 +1,7 @@
 import getpass
 import os
 
-#grasp_info_dir = os.path.expanduser("~") + "/grasp_data"	# The fully qualified path to the grasp data
-grasp_info_dir = "/media/" + getpass.getuser() + "/FA648F24648EE2AD/grasp_data"	# The fully qualified path to the grasp data
+grasp_info_dir = os.path.expanduser("~") + "/grasp_data"	# The fully qualified path to the grasp data
+#grasp_info_dir = "/media/" + getpass.getuser() + "/FA648F24648EE2AD/grasp_data"	# The fully qualified path to the grasp data
 kinect_topic_prefix = "/kinect2/qhd/"
 kinect_data_topics = [kinect_topic_prefix + "image_color_rect/compressed", kinect_topic_prefix + "image_depth_rect/compressed"]
